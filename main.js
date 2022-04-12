bcdb41b6cef6e76698fb2605aeb3a76aed039173
404goblintime <79607814+404goblintime@users.noreply.github.com>
2022-04-11 18:45:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(1300, 800);
+    canvas.position(560, 100);}
+    function draw() {
+        background('#EC5800');
+    }
